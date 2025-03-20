a44995adef7a6149b4c106c3d822b0f8d65535c6
DarkNaku <skyrack@naver.com>
2025-03-20 13:24:42 +0900

    Popup 참조 수정

--- Assets/View/Runtime/View.cs
+++ Assets/View/Runtime/View.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-#if DARKNAKU_POPUP
-using DarkNaku.Popup;
-#endif
-
 #if UNITY_EDITOR
 using UnityEditor;
 using System.Reflection;
@@ -44,36 +40,6 @@ namespace DarkNaku.View {
         private Dictionary<string, IViewHandler> _viewTable = null;
         private static readonly object _lock = new();
         private static View _instance;
-
-#if UNITY_EDITOR
-        [InitializeOnLoadMethod]
-        private static void PackageImportHandler() {
-            var define = $"DARKNAKU_{MethodBase.GetCurrentMethod().DeclaringType.Assembly.GetName().Name.ToUpper()}";
-
-            System.Array buildTargets = System.Enum.GetValues(typeof(BuildTarget));
-
-            foreach (BuildTarget target in buildTargets) {
-                var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(target);
-
-                if (BuildPipeline.IsBuildTargetSupported(buildTargetGroup, target) == false) continue;
-
-#if UNITY_2023_1_OR_NEWER
-                var namedBuildTarget = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(buildTargetGroup);
-                var defines = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
-#else
-                var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
-#endif
-
-                if (defines.IndexOf(define) > 0) continue;
-
-#if UNITY_2023_1_OR_NEWER
-				PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, $"{defines};{define}".Replace(";;", ";"));
-#else
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, $"{defines};{define}".Replace(";;", ";"));
-#endif
-            }
-        }
-#endif
 
         public static IViewHandler Change(string viewName) {
             return Instance._Change<IViewHandler>(viewName);
@@ -150,7 +116,7 @@ namespace DarkNaku.View {
             CurrentView = handler;
 
 #if DARKNAKU_POPUP
-            Popup.MainCanvas = CurrentView.ViewCanvas;
+            Popup.Popup.MainCanvas = CurrentView.ViewCanvas;
 #endif
 
             yield return CurrentView.Show();
